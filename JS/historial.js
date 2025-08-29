45bdc3d6dcba458fc410a5c7c62eeeb5302d4d58
LuisDavidGR <ldavidgrdrmz@gmail.com>
2025-08-29 15:37:54 -0700

    feat: Organizacion de estilos, scripts y html

--- JS/historial.js
+++ JS/historial.js
@@ -13,12 +13,3 @@ function borrarHistorial(){
 }
 
 
-/*let recibirHistorial = sessionStorage.getItem("reciboHistorial")
-
-let recibo = document.createElement("div") 
-recibo.className="acumulador-recibos"
-
-recibo.innerHTML = recibirHistorial // creacion de html del recibo
-document.getElementById("recibos").append(recibo)
-*/
-
