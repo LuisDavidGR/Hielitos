f1c4290aa8a24330e3136580c7801d858ff72186
LuisDavidGR <ldavidgrdrmz@gmail.com>
2025-08-31 14:07:05 -0700

    feat: Mejora en el apartado de historial, se implemento un mejor grid y se quito el desbordamiento

--- JS/historial.js
+++ JS/historial.js
@@ -1,10 +1,11 @@
 let contenedorRecibos = document.getElementById("recibos")
 let historialRecibos = sessionStorage.getItem("historalRecibos")
+let contenedorVacio = document.getElementById("div-vacio")
 
 if(historialRecibos){
     contenedorRecibos.innerHTML = historialRecibos
 } else{
-    contenedorRecibos.innerHTML = "<p>No hay recibos</p>"
+    contenedorVacio.innerHTML = "<p>No hay recibos disponibles</p>"
 }
 
 function borrarHistorial(){
@@ -12,4 +13,14 @@ function borrarHistorial(){
     location.reload();
 }
 
+function descargarRecibos(){
+    const elemento = document.getElementById("recibos");
 
+    html2canvas(elemento).then(canvas => {
+        // Crear un enlace para descargar la imagen
+        const enlace = document.createElement("a");
+        enlace.download = "Recibos.png"; // Nombre del archivo
+        enlace.href = canvas.toDataURL("image/png"); // Formato PNG
+        enlace.click(); // Simular clic para descargar
+    });
+}
